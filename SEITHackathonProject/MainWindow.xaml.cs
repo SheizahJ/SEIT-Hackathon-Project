6f522a3b92dfc814d72778e34f78d5626dae0559
SheizahJ <sheizah.jim@gmail.com>
2025-03-22 22:24:38 -0400

    Basic UI Framework
    - Added an alert box for future notices to the route.
    - Main Route Info menu where the user can select between tabs "Current Route" and "Suggested Routes"
    - Small animation on closing/opening the Route Info menu
    
    Ignore Graph.cs. Will be deleted in the future as it is AI generated for the purpose of understanding the Dijkstra Algorithm.

--- SEITHackathonProject/MainWindow.xaml.cs
+++ SEITHackathonProject/MainWindow.xaml.cs
@@ -9,20 +9,86 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace SEITHackathonProject
 {
+
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
+        // constants
+        private SolidColorBrush TabSelectColor = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FFB8B4B4"));
+        private const int InfoUpYPos = 80;
+        private const int InfoDownYPos = 470;
+
+        // variables
+        private bool routeInfoShown = true;
+
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void mapView_Loaded(object sender, RoutedEventArgs e)
+        {
+            GMap.NET.GMaps.Instance.Mode = GMap.NET.AccessMode.ServerAndCache;
+            // choose your provider here
+            mapView.MapProvider = GMap.NET.MapProviders.OpenStreetMapProvider.Instance;
+            mapView.MinZoom = 2;
+            mapView.MaxZoom = 17;
+            // whole world zoom
+            mapView.Zoom = 2;
+            // lets the map use the mousewheel to zoom
+            mapView.MouseWheelZoomType = GMap.NET.MouseWheelZoomType.MousePositionAndCenter;
+            // lets the user drag the map
+            mapView.CanDragMap = true;
+            // lets the user drag the map with the left mouse button
+            mapView.DragButton = MouseButton.Left;
+        }
+
+
+        // UI Events - Purely for visual aspects
+        private void InfoShowBar_Click(object sender, RoutedEventArgs e)
+        {
+            void TransformAnimation(int toPosition)
+            {
+                DoubleAnimation animY = new DoubleAnimation
+                {
+                    From = RouteInfoTranslation.Y,
+                    To = toPosition, // Translate to 100 on the Y axis
+                    Duration = new Duration(TimeSpan.FromSeconds(0.3))
+                };
+
+                // Apply the animations to the TranslateTransform
+                RouteInfoTranslation.BeginAnimation(TranslateTransform.YProperty, animY);
+            }
+
+
+            routeInfoShown = !(routeInfoShown);
+
+            if (routeInfoShown)
+                TransformAnimation(InfoUpYPos);
+            else
+                TransformAnimation(InfoDownYPos);
+        }
+
+        private void CrrntRouteBtn_Click(object sender, RoutedEventArgs e)
+        {
+            CrrntRouteBtn.Background = TabSelectColor;
+            SuggstRouteBtn.Background = null;
+        }
+
+        private void SuggstRouteBtn_Click(object sender, RoutedEventArgs e)
+        {
+            SuggstRouteBtn.Background = TabSelectColor;
+            CrrntRouteBtn.Background = null;
         }
     }
 }
